ded3a2b425127a14321d498e90978dcc950a0618
Kenshin_Bryz <84550697+BryLim-AI@users.noreply.github.com>
2023-05-20 10:49:45 +0800

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,93 @@
+<script setup>
+</script>
+
+<template>
+  <h2>AlphaChainz Blockchain Web 2023</h2>
+
+  <div class="block-contents">
+
+  <div class="block-blocks">
+    <img width="40" height="40" src="https://img.icons8.com/arcade/40/server.png" alt="server"/>
+    <p>Open Source Servers</p>
+  </div>
+  <div class="block-blocks">
+    <img width="40" height="40" src="https://img.icons8.com/fluency/40/conference-call.png" alt="conference-call"/>
+    <p>Community</p>
+  </div>
+  <div class="block-blocks">
+    <img width="40" height="40" src="https://img.icons8.com/fluency/40/apps-tab.png" alt="apps-tab"/>
+    <p>dApps List</p>
+  </div>
+  <div class="block-blocks">
+    <img width="40" height="40" src="https://img.icons8.com/dusk/40/news.png" alt="news"/>
+    <p>News</p>
+  </div>
+  <div class="block-blocks">
+    <img width="40" height="40" src="https://img.icons8.com/color/40/bitcoin--v1.png" alt="bitcoin--v1"/>
+    <p>CryptoCurrency</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+  <div class="block-blocks">
+    <p>Lorem Ipsum</p>
+  </div>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
